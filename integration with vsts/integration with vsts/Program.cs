88f25219a280b88084b887250a6079509945fcea
Vishal Khatal <vishal.khatal2206@gmail.com>
2018-07-11 17:57:12 +0530

    Added VSTS first API

--- integration with vsts/integration with vsts/Program.cs
+++ integration with vsts/integration with vsts/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,12 +12,45 @@ namespace integration_with_vsts
     {
         static void Main(string[] args)
         {
+            GetProjects();
            GetWorkItem();
         }
         public static void GetWorkItem()
         {
 
         }
+
+        public static async void GetProjects()
+        {
+            try
+            {
+                var personalaccesstoken = "Its my Personal";
+
+                using (HttpClient client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Accept.Add(
+                        new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+                        Convert.ToBase64String(
+                            System.Text.ASCIIEncoding.ASCII.GetBytes(
+                                string.Format("{0}:{1}", "", personalaccesstoken))));
+
+                    using (HttpResponseMessage response = client.GetAsync(
+                                "https://vso-lis.visualstudio.com/DefaultCollection/_apis/projects").Result)
+                    {
+                        response.EnsureSuccessStatusCode();
+                        string responseBody = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine(responseBody);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
     }
     
 }
