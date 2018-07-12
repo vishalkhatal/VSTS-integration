80938be6c79dcf18caa7f0b176ccfd97b295ece7
Vishal Khatal <vishal.khatal2206@gmail.com>
2018-07-12 14:02:54 +0530

    Added few more APIs

--- integration with vsts/integration with vsts/Program.cs
+++ integration with vsts/integration with vsts/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
 using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
 using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,7 +16,10 @@ namespace integration_with_vsts
     {
         static void Main(string[] args)
         {
+            RunGetBugsQueryUsingBugTitle();
+            QueryForWorkItem();
             RunGetBugsQueryUsingClientLib();
+            SearchBug();
             GetProjects();
             GetWorkItem();
         }
@@ -113,9 +117,13 @@ namespace integration_with_vsts
                 {
                     //need to get the list of our work item ids and put them into an array
                     List<int> list = new List<int>();
+                    int counter = 0;
                     foreach (var item in workItemQueryResult.WorkItems)
                     {
+                        if (counter > 150)
+                            break;
                         list.Add(item.Id);
+                        counter++;
                     }
                     int[] arr = list.ToArray();
 
@@ -143,8 +151,207 @@ namespace integration_with_vsts
             }
 
         }
-    }
 
+        public static void SampleREST()
+        {
+            Uri collectionUri = new Uri("https://vso-lis.visualstudio.com");
+            string personalAccessToken = "oqrryl7tr2c73fobfq7b72sxpacfq452fxppfmepg5kcdalp4ata";
+            string teamProjectName = "SDB20";
+            // Create a connection object, which we will use to get httpclient objects.  This is more robust
+            // then newing up httpclient objects directly.  Be sure to send in the full collection uri.
+            // For example:  http://myserver:8080/tfs/defaultcollection
+            // We are using default VssCredentials which uses NTLM against a Team Foundation Server.  See additional provided
+            // examples for creating credentials for other types of authentication.
+            VssConnection connection = new VssConnection(collectionUri, new VssCredentials());
+
+            // Create instance of WorkItemTrackingHttpClient using VssConnection
+            WorkItemTrackingHttpClient witClient = connection.GetClient<WorkItemTrackingHttpClient>();
+
+            // Get 2 levels of query hierarchy items
+            List<QueryHierarchyItem> queryHierarchyItems = witClient.GetQueriesAsync(teamProjectName, depth: 2).Result;
+
+            // Search for 'My Queries' folder
+            QueryHierarchyItem myQueriesFolder = queryHierarchyItems.FirstOrDefault(qhi => qhi.Name.Equals("My Queries"));
+            if (myQueriesFolder != null)
+            {
+                string queryName = "REST Sample";
+
+                // See if our 'REST Sample' query already exists under 'My Queries' folder.
+                QueryHierarchyItem newBugsQuery = null;
+                if (myQueriesFolder.Children != null)
+                {
+                    newBugsQuery = myQueriesFolder.Children.FirstOrDefault(qhi => qhi.Name.Equals(queryName));
+                }
+                if (newBugsQuery == null)
+                {
+                    // if the 'REST Sample' query does not exist, create it.
+                    newBugsQuery = new QueryHierarchyItem()
+                    {
+                        Name = queryName,
+                        Wiql = "SELECT [System.Id],[System.WorkItemType],[System.Title],[System.AssignedTo],[System.State],[System.Tags] FROM WorkItems WHERE [System.TeamProject] = @project AND [System.WorkItemType] = 'Bug' AND [System.State] = 'New'",
+                        IsFolder = false
+                    };
+                    newBugsQuery = witClient.CreateQueryAsync(newBugsQuery, teamProjectName, myQueriesFolder.Name).Result;
+                }
+
+                // run the 'REST Sample' query
+                WorkItemQueryResult result = witClient.QueryByIdAsync(newBugsQuery.Id).Result;
+
+                if (result.WorkItems.Any())
+                {
+                    int skip = 0;
+                    const int batchSize = 100;
+                    IEnumerable<WorkItemReference> workItemRefs;
+                    do
+                    {
+                        workItemRefs = result.WorkItems.Skip(skip).Take(batchSize);
+                        if (workItemRefs.Any())
+                        {
+                            // get details for each work item in the batch
+                            List<WorkItem> workItems = witClient.GetWorkItemsAsync(workItemRefs.Select(wir => wir.Id)).Result;
+                            foreach (WorkItem workItem in workItems)
+                            {
+                                // write work item to console
+                                Console.WriteLine("{0} {1}", workItem.Id, workItem.Fields["System.Title"]);
+                            }
+                        }
+                        skip += batchSize;
+                    }
+                    while (workItemRefs.Count() == batchSize);
+                }
+                else
+                {
+                    Console.WriteLine("No work items were returned from query.");
+                }
+            }
+        }
+
+        public static async void SearchBug()
+        {
+            try
+            {
+                var personalaccesstoken = "oqrryl7tr2c73fobfq7b72sxpacfq452fxppfmepg5kcdalp4ata";
+
+                using (HttpClient client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Accept.Add(
+                        new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+                        Convert.ToBase64String(
+                            System.Text.ASCIIEncoding.ASCII.GetBytes(
+                                string.Format("{0}:{1}", "", personalaccesstoken))));
+
+                    using (HttpResponseMessage response = client.GetAsync(
+                                "https://vso-lis.visualstudio.com/SDB20/_apis/wit/queries?$filter=ReadAsync&$top=10&$expand=all&$includeDeleted=true&api-version=4.1").Result)
+                    {
+                        response.EnsureSuccessStatusCode();
+                        string responseBody = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine(responseBody);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public static async void QueryForWorkItem()
+        {
+            try
+            {
+                var personalaccesstoken = "oqrryl7tr2c73fobfq7b72sxpacfq452fxppfmepg5kcdalp4ata";
+                Uri uri = new Uri("https://vso-lis.visualstudio.com");
+                string personalAccessToken = "oqrryl7tr2c73fobfq7b72sxpacfq452fxppfmepg5kcdalp4ata";
+                string project = "SDB20";
+
+                VssBasicCredential credentials = new VssBasicCredential("", personalAccessToken);
+                // 3. Query for Work Items
+                using (WorkItemTrackingHttpClient workItemTrackingHttpClient = new WorkItemTrackingHttpClient(uri, credentials))
+                {
+                    // 3.1 .. using a Wiql (Work Item Query Language) Query
+                    var wiqlQuery = new Wiql() { Query = "Select * from WorkItems" };
+                    var workItemQueryResultForWiqlBasedQuery = workItemTrackingHttpClient.QueryByWiqlAsync(wiqlQuery).Result;
+
+                    var workItemsForQueryResultForWiqlBasedQuery = workItemTrackingHttpClient
+                        .GetWorkItemsAsync(
+                            workItemQueryResultForWiqlBasedQuery.WorkItems.Select(workItemReference => workItemReference.Id),
+                            expand: WorkItemExpand.All).Result;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public static List<WorkItem> RunGetBugsQueryUsingBugTitle()
+        {
+            Uri uri = new Uri("https://vso-lis.visualstudio.com");
+            string personalAccessToken = "oqrryl7tr2c73fobfq7b72sxpacfq452fxppfmepg5kcdalp4ata";
+            string project = "SDB20";
+
+            VssBasicCredential credentials = new VssBasicCredential("", personalAccessToken);
+
+            //create a wiql object and build our query
+            Wiql wiql = new Wiql()
+            {
+                Query = "Select [State], [Title] " +
+                        "From WorkItems " +
+                        "Where [System.TeamProject] = '" + project + "' " +
+                        "And [System.Title] contains 'ReadAsync' " +
+                        "And [System.State] <> 'Closed' " +
+                        "Order By [State] Asc, [Changed Date] Desc"
+            };
+
+            //create instance of work item tracking http client
+            using (WorkItemTrackingHttpClient workItemTrackingHttpClient = new WorkItemTrackingHttpClient(uri, credentials))
+            {
+                //execute the query to get the list of work items in the results
+                WorkItemQueryResult workItemQueryResult = workItemTrackingHttpClient.QueryByWiqlAsync(wiql).Result;
+
+                //some error handling                
+                if (workItemQueryResult.WorkItems.Count() != 0)
+                {
+                    //need to get the list of our work item ids and put them into an array
+                    List<int> list = new List<int>();
+                    int counter = 0;
+                    foreach (var item in workItemQueryResult.WorkItems)
+                    {
+                        if (counter > 150)
+                            break;
+                        list.Add(item.Id);
+                        counter++;
+                    }
+                    int[] arr = list.ToArray();
+
+                    //build a list of the fields we want to see
+                    string[] fields = new string[3];
+                    fields[0] = "System.Id";
+                    fields[1] = "System.Title";
+                    fields[2] = "System.State";
+
+                    //get work items for the ids found in query
+                    var workItems = workItemTrackingHttpClient.GetWorkItemsAsync(arr, fields, workItemQueryResult.AsOf).Result;
+
+                    Console.WriteLine("Query Results: {0} items found", workItems.Count);
+
+                    //loop though work items and write to console
+                    foreach (var workItem in workItems)
+                    {
+                        Console.WriteLine("{0}          {1}                     {2}", workItem.Id, workItem.Fields["System.Title"], workItem.Fields["System.State"]);
+                    }
+
+                    return workItems;
+                }
+
+                return null;
+            }
+
+        }
+
+    }
 }
-//install-Package Microsoft.TeamFoundationServer.Client -Version 15.112.1
-//    Install-Package Microsoft.VisualStudio.Services.Client -Version 15.112.1
+// Install-Package Microsoft.TeamFoundationServer.Client -Version 15.112.1
+// Install-Package Microsoft.VisualStudio.Services.Client -Version 15.112.1
